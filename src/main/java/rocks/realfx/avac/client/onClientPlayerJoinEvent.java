139f6aad147b97aba0cb0ff3cb3f037af29b7a89
leah <rlfx@realfx.rocks>
2024-04-11 17:28:39 +0200

    remove JavaFX dependency

--- src/main/java/rocks/realfx/avac/client/onClientPlayerJoinEvent.java
+++ src/main/java/rocks/realfx/avac/client/onClientPlayerJoinEvent.java
@@ -1,8 +1,5 @@
 package rocks.realfx.avac.client;
 
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.control.Alert;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.payload.CustomPayload;
@@ -14,7 +11,7 @@ import rocks.realfx.avac.avacState;
 import rocks.realfx.avac.common.avacPayload;
 import rocks.realfx.avac.common.validatePayload;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class onClientPlayerJoinEvent implements ClientPlayConnectionEvents.Join {
 
@@ -34,23 +31,10 @@ public class onClientPlayerJoinEvent implements ClientPlayConnectionEvents.Join 
 		ClientPlayNetworking.getSender().sendPacket(ClientPlayNetworking.createC2SPacket(clientInfo));
 
 		if (!clientValidated) {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-				AvAC.LOGGER.error(String.valueOf(e));
-            }
-
-			new JFXPanel();
-
-			Platform.runLater(() -> {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("FATAL ERROR");
-				alert.setHeaderText("AN INTENTIONAL ERROR HAS OCCURRED.");
-                alert.setContentText("Error code: 2");
-                alert.showAndWait();
-				client.scheduleStop();
-                Platform.exit();
-            });
+			JOptionPane.showMessageDialog(
+				null,
+				"An intentional error has occurred. Please contact an administrator.\nError code: 2"
+			);
 		}
 
 	}
